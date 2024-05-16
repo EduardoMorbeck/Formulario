fc481c46be8433c89b5fc290d7364f3683166d00
Eduardo S. Morbeck <eduardomorbeck187@gmail.com>
2024-05-16 13:31:34 -0300

    Update

--- script.js
+++ script.js
@@ -63,7 +63,7 @@ function checkPassword(){
     if(passwordValue === ""){
         errorInput(password, "Digite uma senha")
     }else if(passwordValue.length < 8){
-        errorInput(password, "A senha precisa ter no mínimo 8 caracteres")
+        errorInput(password, "No mínimo 8 caracteres")
     }else{
         const formItem = password.parentElement
         formItem.className = "input-box"
